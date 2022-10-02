973219aae6fb13fcd7c3620576ac4d602e9e2e5d
Brandi Cameron <hello@brandicameron.com>
2022-10-02 14:30:10 -0500

    Created joke component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header/Header';
+import Joke from './components/Joke/Joke';
 
 function App() {
   return (
     <>
       <Header />
+      <Joke />
     </>
   );
 }
---
+++ src/components/Joke/Joke.js
@@ -0,0 +1,9 @@
+import styles from './Joke.module.css';
+
+export default function Joke() {
+  return (
+    <main className={styles.jokeContainer}>
+      <h1>Sample joke here.</h1>
+    </main>
+  );
+}
