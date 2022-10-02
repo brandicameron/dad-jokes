eca2838005386e07f972a16b007cb52c375ebac0
Brandi Cameron <hello@brandicameron.com>
2022-10-02 14:26:53 -0500

    Created header with logo and history button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Header from './components/Header/Header';
+
 function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import styles from './Header.module.css';
+import LogoText from '../../assets/dad-jokes-text.svg';
+import Dad1 from '../../assets/dad-1.png';
+import Dad2 from '../../assets/dad-2.png';
+import Dad3 from '../../assets/dad-3.png';
+import HistoryIcon from '../../assets/history.svg';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <a href='/' className={styles.logo}>
+        <img className={styles.dad1} src={Dad1} alt='Dad Jokes' />
+        <img className={styles.dad2} src={Dad2} alt='Dad Jokes' />
+        <img className={styles.logoText} src={LogoText} alt='Dad Jokes' />
+        <img className={styles.dad3} src={Dad3} alt='Dad Jokes' />
+      </a>
+      <button className={styles.historyBtn} aria-label='Click to View Joke History'>
+        <img src={HistoryIcon} alt='' />
+      </button>
+    </header>
+  );
+}
